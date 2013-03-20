43c83151822f3c8bb464b125160bddef948d10d3
Bhaskar <bhaskar@pslblrlt049-ThinkPad-SL410.(none)>
2013-03-20 18:17:11 +0530

    new api is added

--- rest-api/server.js
+++ rest-api/server.js
@@ -2,4 +2,5 @@ var express = require('express');
 var app = express();
 var accountservice = require('./services/accountservice.js');
 app.get('/getaccounts', accountservice.getaccounts);
+app.get('/getservicesbyaccount', accountservice.getservicesbyaccount);
 app.listen(8080);
