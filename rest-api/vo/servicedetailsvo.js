0cb68f4422b0b3f18740f1290e861cca9a5a20de
Bhaskar <bhaskar@pslblrlt049-ThinkPad-SL410.(none)>
2013-04-16 17:26:29 +0530

    value object is added

---
+++ rest-api/vo/servicedetailsvo.js
@@ -0,0 +1,10 @@
+var servicedetailsvo =function(id,name,imagename,servicedisplaymap){
+  this.id =id;
+  this.name = name;
+  this.imagename = imagename;
+  this.servicedisplaymap = servicedisplaymap;
+}
+
+// node.js module export
+module.exports = servicedetailsvo;
+
